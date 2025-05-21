3a236c7d4b187c07c7bdb9a6958b43e684b58437
Kiraakatsuki <kiraakatsukiofficial@gmail.com>
2025-05-21 10:58:18 +0530

    first commit

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template # importing various modules from flask
+from flask import Flask,render_template,request # importing various modules from flask
 app=Flask(__name__) #creating an instance using variable
 @app.route('/') # to call or hit the function when the mentioned symbol is called. @ is used for decoration
 def index() :
@@ -8,6 +8,12 @@ def index() :
 @app.route('/product') # to call or hit the function when the mentioned symbol is called. @ is used for decoration
 def product() :
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login() :
+    if request.method=="POST":
+        print(request.form['first'])
+    return render_template('login.html')
 
 
 if __name__ == '__main__' : # confirming whether the page is main page or not
